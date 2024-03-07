0f30782ac184fac29a7a8fe688e216890e9aad6f
vic <victor.li.7802@gmail.com>
2024-03-07 16:35:51 +0000

    not sure whats wrong, getting 500 error

--- prh/prh/settings.py
+++ prh/prh/settings.py
@@ -24,7 +24,7 @@ ROBOTSTXT_OBEY = False
 LOG_LEVEL = 'INFO'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -74,7 +74,7 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
